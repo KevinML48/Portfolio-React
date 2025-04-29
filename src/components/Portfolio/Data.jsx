98f87d467577002f4f6a5932753bf6b745d6e5a4
Kevin <kevinpromogreta@gmail.com>
2025-04-29 14:07:17 +0200

    Clean up

--- src/components/Portfolio/Data.jsx
+++ src/components/Portfolio/Data.jsx
@@ -1,16 +1,31 @@
-// (en haut du fichier)
 import Work1 from "../../assets/Aten.jpg";
-import Work2 from "../../assets/work2.jpg";
-import Work3 from "../../assets/work3.jpg";
-import Work4 from "../../assets/work4.jpg";
-import Work5 from "../../assets/work5.jpg";
-import Work6 from "../../assets/work6.jpg";
-import Work7 from "../../assets/work7.jpg";
-import Work8 from "../../assets/work8.jpg";
-// Pour test visuel
 import TestImg from "../../assets/behance_template_aten.jpg"; // <- une image très différente
 
 export const projectsData = [
+  {
+    id: 1,
+    image: Work1,
+    modalImage: TestImg, // Spécifique pour la modal
+    title: "Web design",
+    category: "app",
+    slug: "work-1",
+  },
+  {
+    id: 1,
+    image: Work1,
+    modalImage: TestImg, // Spécifique pour la modal
+    title: "Web design",
+    category: "app",
+    slug: "work-1",
+  },
+  {
+    id: 1,
+    image: Work1,
+    modalImage: TestImg, // Spécifique pour la modal
+    title: "Web design",
+    category: "app",
+    slug: "work-1",
+  },
   {
     id: 1,
     image: Work1,
